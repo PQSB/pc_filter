3746496555d1f5c87695e58b1e5277e856602ee4
Andres <a.galea.2022@alumnos.urjc.es>
2026-04-09 19:35:17 +0200

    Update pc_filter_lib.cpp adding parameters to fov filter function

--- src/pc_filter/pc_filter_lib.cpp
+++ src/pc_filter/pc_filter_lib.cpp
@@ -8,12 +8,11 @@ void pc_filter::filter_cloud_x(const sensor_msgs::msg::PointCloud2::SharedPtr ms
   std_msgs::msg::Header original_header = msg->header;
 
   // Convertir la nube de puntos a PCL
-  pcl::PCLPointCloud2 pcl_pc2;
-  pcl_conversions::toPCL(*msg, pcl_pc2);
   // Convertir de PCLPointCloud2 a PointCloud<pcl::PointXYZ>
   // pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
+  pcl::fromROSMsg(*msg, *cloud);
+ 
   // Filtrar la nube de puntos
   // pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZI>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -24,16 +23,17 @@ void pc_filter::filter_cloud_x(const sensor_msgs::msg::PointCloud2::SharedPtr ms
       filtered_cloud->points.push_back(point);
     }
   }
-  // Convertir la nube de puntos filtrada a PCLPointCloud2
-  pcl::PCLPointCloud2 pcl_filtered_pc2;
-  pcl::toPCLPointCloud2(*filtered_cloud, pcl_filtered_pc2);
-  // Convertir de PCLPointCloud2 a sensor_msgs/PointCloud2
-  pcl_conversions::fromPCL(pcl_filtered_pc2, filtered_msg);
+
+  // Convertir la nube de puntos filtrada a sensor_msgs/PointCloud2
+  pcl::toROSMsg(*filtered_cloud, filtered_msg);
 
   filtered_msg.header = original_header;
 }
 
-void pc_filter::filter_cloud_fov_2d(const sensor_msgs::msg::PointCloud2::SharedPtr msg, sensor_msgs::msg::PointCloud2 &filtered_msg)
+void pc_filter::filter_cloud_fov_2d(
+  const sensor_msgs::msg::PointCloud2::SharedPtr msg,
+  sensor_msgs::msg::PointCloud2 &filtered_msg,
+  float min_angle, float max_angle)
 {
   std_msgs::msg::Header original_header = msg->header;
 
@@ -50,8 +50,7 @@ void pc_filter::filter_cloud_fov_2d(const sensor_msgs::msg::PointCloud2::SharedP
 
     float angle = std::atan2(point.y, point.x);
 
-    // Frontal FOV de 180°: [-90°, +90°]
-    if (angle > -M_PI/2 && angle < M_PI/2)
+    if (angle > min_angle && angle < max_angle)
     {
       filtered_cloud->points.push_back(point);
     }
