baca35a6a3b8aba90e119749c821ecf718d3b7b3
Andres <a.galea.2022@alumnos.urjc.es>
2026-05-25 23:04:21 +0200

    Add pc filter function to hpp file

--- include/pc_filter/pc_filter_lib.hpp
+++ include/pc_filter/pc_filter_lib.hpp
@@ -3,6 +3,18 @@
 
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include <cmath>
+#include <string>
+#include <memory>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+struct Detection {
+    std::string category;
+    float x, y, z;
+    float w, l, h;
+    float ry;
+    float score;
+};
 
 namespace pc_filter
 {
@@ -16,5 +28,12 @@ void filter_cloud_fov_2d(
     sensor_msgs::msg::PointCloud2 &filtered_msg,
     float min_angle = -M_PI/2.0f, float max_angle = M_PI/2.0f);
 
+int
+filter_detections_from_cloud(
+    const std::shared_ptr<std::vector<Detection>>& dets,
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+    const bool from_camera);
+
 }
+
 #endif  // PC_FILTER__PC_FILTER_LIB_HPP_
