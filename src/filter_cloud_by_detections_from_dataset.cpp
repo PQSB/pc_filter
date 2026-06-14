0411e0dafec0ccf2556b17133037674230e357ba
Andres <a.galea.2022@alumnos.urjc.es>
2026-06-14 06:03:57 +0200

    Export images and input point clouds done

--- src/filter_cloud_by_detections_from_dataset.cpp
+++ src/filter_cloud_by_detections_from_dataset.cpp
@@ -4,12 +4,29 @@
 #include <filesystem>
 #include <fstream>
 
+#include "pc_filter/pc_filter_lib.hpp"
+
 // ros2 includes
 #include <rosbag2_cpp/writer.hpp>
 #include <rosbag2_storage/storage_options.hpp>
 #include <rosbag2_cpp/converter_options.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <std_msgs/msg/header.hpp>
+
+// pcl includes
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h> 
+
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.hpp>
 
 #include <cxxopts.hpp>
+
+// De las funciones temporales
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/extract_indices.h>
 
 namespace fs = std::filesystem;
 
@@ -36,6 +53,124 @@ struct Config
     std::string filtered_topic;
     std::vector<std::string> classes;
 };
+
+/*
+FUNCIÓN LEER DETECCIONES DE FICHERO TEMPORAL A FALTA DE MODIFICAR LA DE LA LIBRERÍA
+*/
+std::vector<Detection>
+load_detections_from_file(
+        const std::string& det_path,
+        const float min_score,
+        const std::unordered_set<std::string>& allowed_set)
+{
+    std::vector<Detection> detections;
+
+    // Argument validation
+    if (!std::filesystem::exists(det_path)) {
+        std::cerr << "[WARN] Detections file: " << det_path << " does not exists\n";
+        return detections;
+    }
+
+    std::ifstream file(det_path);
+    if (!file.is_open()) {
+        std::cerr << "[ERROR] Detections file: " << det_path << "can't be oppened\n";
+        return detections;
+    }
+
+    std::string line;
+    std::stringstream ss;
+
+    while (std::getline(file, line)) {
+        if (line.empty()) continue;
+
+        ss.str(line);
+        ss.clear();
+
+        Detection det;
+
+        if (ss >> det.category >> det.x >> det.y >> det.z
+            >> det.w >> det.l >> det.h >> det.ry >> det.score) {
+
+            if (det.score >= min_score && allowed_set.find(det.category) != allowed_set.end()) {
+                detections.push_back(det);
+            }
+
+        }
+    }
+
+    return detections;
+}
+
+
+/*
+FUNCIÓN FILTRADO TEMPORAL A FALTA DE MODIFICAR LA DE LA LIBRERÍA
+*/
+int
+filter_detections_from_cloud(
+  const std::vector<Detection>& dets,
+  pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud)
+{
+    // 1. Filtros de seguridad para evitar caídas del programa
+    if (dets.empty() || !cloud || cloud->empty()) {
+        return -1;
+    }
+
+    // 2. Vector para acumular los índices de puntos a borrar de TODAS las cajas
+    pcl::IndicesPtr indices_to_remove(new std::vector<int>);
+
+    // 3. Configurar el objeto CropBox apuntando a nuestra nube de entrada
+    pcl::CropBox<pcl::PointXYZI> crop;
+    crop.setInputCloud(cloud);
+
+    for (const auto& det : dets)
+    {
+
+        Eigen::Vector4f min_pt, max_pt;
+        Eigen::Vector3f rotation;
+        Eigen::Vector3f translation(det.x, det.y, det.z);
+
+        // X=Largo, Y=Ancho, Z=Alto. Rotation in Z.
+        min_pt = Eigen::Vector4f(-det.l / 2.0f, -det.w / 2.0f, -det.h / 2.0f, 1.0f);
+        max_pt = Eigen::Vector4f( det.l / 2.0f,  det.w / 2.0f,  det.h / 2.0f, 1.0f);
+   
+        rotation = Eigen::Vector3f(0.0f, 0.0f, det.ry);
+
+        crop.setMin(min_pt);
+        crop.setMax(max_pt);
+        crop.setTranslation(translation);
+        crop.setRotation(rotation);
+
+        std::vector<int> inside;
+        crop.filter(inside);
+
+        indices_to_remove->insert(indices_to_remove->end(), inside.begin(), inside.end());
+    }
+
+    // 7. Si ninguna caja contenía puntos, terminamos el proceso de inmediato
+    if (indices_to_remove->empty()) {
+        // std::cerr << "Nothing filtered in the detections provided\n";
+        //std::cout << "\n[Filtro] Terminado. No se encontraron puntos dentro de las cajas (0 filtrados)." << std::endl;
+        cloud->clear();
+        return 0;
+    }
+
+    std::sort(indices_to_remove->begin(), indices_to_remove->end());
+    indices_to_remove->erase(std::unique(indices_to_remove->begin(), indices_to_remove->end()), indices_to_remove->end());
+
+    // 9. Ejecutar ExtractIndices una sola vez para toda la nube
+    pcl::ExtractIndices<pcl::PointXYZI> extract;
+    extract.setInputCloud(cloud);
+    extract.setIndices(indices_to_remove);
+    extract.setNegative(false);  // true = borrar los puntos que están dentro de las cajas
+
+    // Filtrar hacia un contenedor temporal y reasignar a la nube original
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
+    extract.filter(*cloud_filtered);
+    //cloud->swap(*cloud_filtered);
+    *cloud = *cloud_filtered; 
+
+    return 0;
+}
 
 std::vector<int64_t>
 loadTimestamps(const fs::path& ts_file)
@@ -71,6 +206,66 @@ loadTimestamps(const fs::path& ts_file)
     }
 
     return timestamps_ns;
+}
+
+pcl::PointCloud<pcl::PointXYZI>::Ptr
+loadPointCloudXYZI(const std::string& filepath)
+{
+    auto cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZI>>();
+
+    // Check if the file exists
+    if (!std::filesystem::exists(filepath)) {
+        std::cerr << "[WARN] File does not exist: " << filepath << "\n";
+        return cloud;
+    }
+
+    // Get the total size of the file
+    const size_t file_size = std::filesystem::file_size(filepath);
+    const size_t point_size = 4 * sizeof(float); // XYZ = 12 bytes
+
+    if (file_size == 0) {
+        std::cerr << "[ERROR] Empty file: " << filepath << "\n";
+        return cloud;
+    }
+
+    if (file_size % point_size != 0) {
+        std::cerr << "[ERROR] Invalid .bin file size for XYZI format: "
+                  << file_size << " bytes\n";
+        return cloud;
+    }
+
+    // Calculate the number of points of the point cloud
+    const size_t num_points = file_size / point_size;
+
+    std::vector<float> raw(num_points * 4);
+
+    // Open the file
+    std::ifstream file(filepath, std::ios::binary);
+    if (!file.is_open()) {
+        std::cerr << "[ERROR] Cannot open file: " << filepath << "\n";
+        return cloud;
+    }
+
+    // comprobar si puedo usar fread
+    if (!file.read(reinterpret_cast<char*>(raw.data()), file_size)) {
+        std::cerr << "[ERROR] Could not read full file\n";
+        return cloud;
+    }
+
+    cloud->points.resize(num_points);
+
+    for (size_t i = 0; i < num_points; ++i) {
+        cloud->points[i].x = raw[i * 4 + 0];
+        cloud->points[i].y = raw[i * 4 + 1];
+        cloud->points[i].z = raw[i * 4 + 2];
+        cloud->points[i].intensity = raw[i * 4 + 3];
+    }
+
+    cloud->width  = num_points;
+    cloud->height = 1;
+    cloud->is_dense = false;
+
+    return cloud;
 }
 
 void
@@ -298,7 +493,7 @@ createTopics(rosbag2_cpp::Writer& writer, const Config& cfg)
         topic_info.offered_qos_profiles = {};
 
         writer.create_topic(topic_info);
-        std::cout << "[OK] " << cfg.pc_topic << " topic created succesfully\n";
+        // std::cout << "[OK] " << cfg.pc_topic << " topic created succesfully\n";
     }
 
     if (cfg.use_detections) {
@@ -307,7 +502,7 @@ createTopics(rosbag2_cpp::Writer& writer, const Config& cfg)
         topic_info.offered_qos_profiles = {};
 
         writer.create_topic(topic_info);
-        std::cout << "[OK] " << cfg.filtered_topic << " topic created succesfully\n";
+        // std::cout << "[OK] " << cfg.filtered_topic << " topic created succesfully\n";
     }
 
     if (cfg.use_images) {
@@ -316,8 +511,154 @@ createTopics(rosbag2_cpp::Writer& writer, const Config& cfg)
         topic_info.offered_qos_profiles = {};
 
         writer.create_topic(topic_info);
-        std::cout << "[OK] " << cfg.img_topic << " topic created succesfully\n";
+        // std::cout << "[OK] " << cfg.img_topic << " topic created succesfully\n";
     }
+
+    std::cout << std::endl;
+}
+
+std::unordered_map<std::string, fs::path>
+buildFileIndex(const fs::path& directory)
+{
+    std::unordered_map<std::string, fs::path> index;
+
+    for (const auto& entry : fs::directory_iterator(directory))
+    {
+        if (!entry.is_regular_file())
+        {
+            continue;
+        }
+
+        index.emplace(
+            entry.path().stem().string(),
+            entry.path());
+    }
+
+    return index;
+}
+
+void
+writeImage(
+    const std::string& img_path,
+    const std::string& topic,
+    rosbag2_cpp::Writer& writer,
+    int64_t timestamp_ns)
+{
+    cv::Mat image = cv::imread(img_path, cv::IMREAD_COLOR);
+
+    if (image.empty()) {
+        throw std::runtime_error("Cannot load image: " + img_path);
+    }
+
+    auto image_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image).toImageMsg();
+
+    rclcpp::Time img_time(timestamp_ns);
+    image_msg->header.frame_id = "cam_2";
+    image_msg->header.stamp = img_time;
+
+    writer.write<sensor_msgs::msg::Image>(*image_msg, topic, img_time);
+}
+
+void
+writeCloud(
+    const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud,
+    const std::string& topic,
+    rosbag2_cpp::Writer& writer,
+    int64_t timestamp_ns)
+{
+    sensor_msgs::msg::PointCloud2 msg;
+
+    pcl::toROSMsg(*cloud,msg);
+
+    rclcpp::Time cloud_time(timestamp_ns);
+
+    msg.header.frame_id = "base_lidar";
+    msg.header.stamp = cloud_time;
+
+    writer.write<sensor_msgs::msg::PointCloud2>(msg, topic, cloud_time);
+}
+
+void
+writeRosbag(
+    rosbag2_cpp::Writer& writer,
+    const Config& cfg,
+    const std::vector<fs::path>& files,
+    const std::vector<int64_t>& timestamps_ns)
+{
+    rclcpp::Serialization<sensor_msgs::msg::PointCloud2> serializer;
+
+
+    std::unordered_map<std::string, fs::path> image_index;
+    std::unordered_map<std::string, fs::path> detection_index;
+
+    if (cfg.use_images)
+    {
+        image_index = buildFileIndex(cfg.img_dir);
+    }
+
+    if (cfg.use_detections)
+    {
+        detection_index = buildFileIndex(cfg.det_dir);
+    }
+
+    std::string frame_id;
+
+    for (size_t i = 0; i < files.size(); ++i)
+    {
+
+        frame_id = files[i].stem().string();
+
+        std::cout
+            << "\rWritting rosbag messages ["
+            << (i + 1)
+            << "/"
+            << files.size()
+            << "]"
+            << std::flush;
+
+        if (cfg.use_images)
+        {
+            auto img_it = image_index.find(frame_id);
+
+            if (img_it != image_index.end()) {
+                writeImage(img_it->second.string(), cfg.img_topic, writer, timestamps_ns[i]);
+
+            } else {
+                throw std::runtime_error("Missing image for frame: " + frame_id);
+            }
+        }
+
+        auto input_cloud = loadPointCloudXYZI(files[i]);
+
+        if (!input_cloud || input_cloud->empty())
+        {
+            std::cerr
+                << "\n[WARN] Empty cloud: "
+                << files[i]
+                << '\n';
+
+            continue;
+        }
+
+        if (cfg.export_input_pc) {
+            writeCloud(input_cloud, cfg.pc_topic, writer, timestamps_ns[i]);
+        }
+
+        if (cfg.use_detections) {
+            auto det_it = detection_index.find(frame_id);
+
+            if (det_it != detection_index.end()) {
+                // Filtrar la nube primero
+                writeCloud(input_cloud, cfg.filtered_topic, writer, timestamps_ns[i]);
+
+            } else {
+                throw std::runtime_error("Missing detections for frame: " + frame_id);
+            }
+        }
+
+    }
+
+    std::cout << '\n';
 }
 
 void
@@ -350,8 +691,8 @@ generate_rosbag(const Config& cfg,  const std::vector<int64_t>& timestamps_ns)
     // Create the topics of the new rosbag
     createTopics(writer, cfg);
 
-
     // Write data in the rosbag
+    writeRosbag(writer, cfg, point_cloud_files, timestamps_ns);
 }
 
 int main(int argc, char* argv[])
@@ -362,13 +703,25 @@ int main(int argc, char* argv[])
 
         validate(cfg);
         print_summary(cfg);
-        std::cout << "[OK] configuration succesful\n";
+        std::cout << "Press [ENTER] to continue, or [ANY KEY + ENTER] to cancel: ";
 
-        std::cout << "[INFO] loading timesatmps..." << std::flush;
+        std::string input;
+        std::getline(std::cin, input);
+
+        if (!input.empty()) {
+            std::cout << "\n[ABORTED] EXECUTION ABORTED\n";
+            exit(EXIT_FAILURE);
+        }
+
+        //std::cout << "[OK] configuration succesful\n";
+
+        //std::cout << "[INFO] loading timesatmps..." << std::flush;
         std::vector<int64_t> timestamps = loadTimestamps(cfg.ts_file);
-        std::cout << "\r[OK] timestamps loaded succesfully\n";
+        //std::cout << "\r[OK] timestamps loaded succesfully\n";
 
         generate_rosbag(cfg, timestamps);
+
+        std::cout << "\n[OK] ROSBAG GENERATED SUCCESFULLY\n";
 
         exit(EXIT_SUCCESS);
     }
